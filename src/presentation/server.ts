1a38b9671f776d048cd5ece5f3a36d12f01e13ce
lucasciappa97@gmail.com <lucasciappa97@gmail.com>
2024-03-14 18:23:34 -0300

    first commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,36 @@
+import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasources';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
+import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource(),
+);
+
+
+export class Server {
+
+  public static start() {
+
+    console.log( 'Server started...' );
+
+    
+    CronService.createJob(
+      '*/5 * * * * *',
+      () => {
+        const url = 'https://google.com';
+        new CheckService(
+          fileSystemLogRepository,
+          () => console.log( `${ url } is ok` ),
+          ( error ) => console.log( error ),
+        ).execute( url );
+        // new CheckService().execute( 'http://localhost:3000' );
+        
+      }
+    );
+
+
+  }
+
+
+}
