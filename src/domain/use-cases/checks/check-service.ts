acce5f921e990eb6970fa85bef8b22c4b228ad11
lucasciappa97@gmail.com <lucasciappa97@gmail.com>
2024-03-15 17:44:13 -0300

    add: nodemailer

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -28,8 +28,11 @@ interface CheckServiceUseCase {
         if ( !req.ok ) {
           throw new Error( `Error on check service ${ url }` );
         }
-  
-        const log = new LogEntity(`Service ${ url } working`, LogSeverityLevel.low);
+        const log = new LogEntity({
+          message: `Service ${ url } working`,
+          level: LogSeverityLevel.low,
+          origin: 'check-service.ts'
+        });
         this.logRepository.saveLog(log)
         this.successCallback && this.successCallback();
 
@@ -38,7 +41,11 @@ interface CheckServiceUseCase {
       } catch (error) {
         
         const errorMessage =  `Service ${ url } is no ok. ${ error }`
-        const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+        const log = new LogEntity({
+          message: errorMessage,
+          level: LogSeverityLevel.high,
+          origin: 'check-service.ts'
+        });
         this.logRepository.saveLog(log);
 
         this.errorCallback && this.errorCallback( errorMessage );
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,16 @@
+import { envs } from '../config/plugins/envs.plugin';
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { SendEmailLogs } from '../domain/use-cases/email/send-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasources';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
-import { CronService } from './cron/cron-service';
+import { CronService } from './cron/cron.service';
+import { EmailService } from './email/email.service';
+
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
 );
+const emailService = new EmailService();
 
 
 export class Server {
@@ -14,20 +19,32 @@ export class Server {
 
     console.log( 'Server started...' );
 
+    //todo: Mandar email
+    // new SendEmailLogs(
+    //   emailService, 
+    //   fileSystemLogRepository,
+    // ).execute(
+    //   ['lucasciappa97@gmail.com','lciapparelli@telecentro.net.ar']
+    // )
+    // emailService.sendEmailWithFileSystemLogs(
+    // ['lucasciappa97@gmail.com','lciapparelli@telecentro.net.ar']
+    // );
     
-    CronService.createJob(
-      '*/5 * * * * *',
-      () => {
-        const url = 'https://google.com';
-        new CheckService(
-          fileSystemLogRepository,
-          () => console.log( `${ url } is ok` ),
-          ( error ) => console.log( error ),
-        ).execute( url );
-        // new CheckService().execute( 'http://localhost:3000' );
+    
+    
+    // CronService.createJob(
+    //   '*/5 * * * * *',
+    //   () => {
+    //     const url = 'https://google.com';
+    //     new CheckService(
+    //       fileSystemLogRepository,
+    //       () => console.log( `${ url } is ok` ),
+    //       ( error ) => console.log( error ),
+    //     ).execute( url );
+    //     // new CheckService().execute( 'http://localhost:3000' );
         
-      }
-    );
+    //   }
+    // );
 
 
   }
