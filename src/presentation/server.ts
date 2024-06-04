2a0c06a26dc79c1f67b63af0f1d3fda8c0a6aec4
Lucas Ciapparelli <lciapparelli@telecentro.net.ar>
2024-06-04 13:59:46 -0300

    add: Mongo data source module

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,16 @@
 import { envs } from '../config/plugins/envs.plugin';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-logs';
-import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasources';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron.service';
 import { EmailService } from './email/email.service';
 
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource(),
+const logRepository = new LogRepositoryImpl(
+  // new FileSystemDatasource(),
+  new MongoLogDatasource(),
 );
 const emailService = new EmailService();
 
@@ -32,19 +34,19 @@ export class Server {
     
     
     
-    // CronService.createJob(
-    //   '*/5 * * * * *',
-    //   () => {
-    //     const url = 'https://google.com';
-    //     new CheckService(
-    //       fileSystemLogRepository,
-    //       () => console.log( `${ url } is ok` ),
-    //       ( error ) => console.log( error ),
-    //     ).execute( url );
-    //     // new CheckService().execute( 'http://localhost:3000' );
+    CronService.createJob(
+      '*/5 * * * * *',
+      () => {
+        const url = 'https://google.com';
+        new CheckService(
+          logRepository,
+          () => console.log( `${ url } is ok` ),
+          ( error ) => console.log( error ),
+        ).execute( url );
+        // new CheckService().execute( 'http://localhost:3000' );
         
-    //   }
-    // );
+      }
+    );
 
 
   }
